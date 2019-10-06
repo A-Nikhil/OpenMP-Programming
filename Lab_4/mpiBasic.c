f3d4eea91803aae2615d5d08298f68d9e2353007
Nikhil Anand <nikhilanand1006@gmail.com>
2019-10-06 19:20:20 +0530

    MPI 1

--- Lab_4/mpiBasic.c
+++ Lab_4/mpiBasic.c
@@ -6,8 +6,7 @@ int main(int argc, char ** argv) {
     char name[80];
     int length;
 
-    MPI_Init(&argc, &argv); // note that argc and argv are passed
-                            // by address
+    MPI_Init(&argc, &argv); // note that argc and argv are passed by address
 
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
     MPI_Comm_size(MPI_COMM_WORLD,&size);
